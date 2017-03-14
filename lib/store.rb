2d3d074dc47276daff1c8ff79279296879bbbeb2
99hsueh <99hhsueh@gmail.com>
2017-03-14 23:34:46 +0000

    complete exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue,
+            numericality: {
+              greater_than_or_equal_to: 0
+            }
+  # validate :apparel
+
+
+  # def apparel
+  #   if(men_apparel = false && women_apparel = false)
+  #     errors.add(men_apparel, women_apparel, "Neither men or women apparel are present")
+  #   end
+  # end
 end
